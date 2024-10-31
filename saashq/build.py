5ea4332cb6325dc4532b743201fe80865b2b002d
SaasHQ <saashqdev@gmail.com>
2024-10-31 16:52:56 -0400

    formatting

--- saashq/build.py
+++ saashq/build.py
@@ -84,7 +84,7 @@ def get_assets_link(saashq_head) -> str:
 
 	if tag:
 		# if tag exists, download assets from github release
-		url = f"https://github.com/saashqdev/saashq/releases/download/{tag}/assets.tar.gz"
+		url = f"https://github.com/saashqdev/saashq/releases/tag/{tag}/assets.tar.gz"
 	else:
 		url = f"http://assets.saashqframework.com/{saashq_head}.tar.gz"
 
